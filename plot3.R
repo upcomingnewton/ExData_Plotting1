c9090cacc9c7fe49332f3457424ae9cbfe1d300f
nitin chadha <contact.nitinchadha@gmail.com>
2014-08-10 16:16:40 +0530

    adding plot3.png plot3.R

--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
 # install sql library
-install.packages("sqldf")
+#install.packages("sqldf")
 library(sqldf)
 
 
@@ -13,7 +13,10 @@ household_data <- read.csv2.sql(fname, my_sql,sep=";")
 household_data$DateTime <- paste(household_data$Date, household_data$Time)
 household_data$DateTime1 <- as.POSIXct(strptime(household_data$DateTime, "%d/%m/%Y %H:%M:%S"))
 
-#plot the data into plot2.png
-png("plot2.png",width=480,height=480)
-plot(household_data$DateTime1,household_data$Global_active_power,  type="l",ylab="Global Active Power (kilowatts)",xlab="")
+#plot the data into plot3.png
+png("plot3.png",width=480,height=480)
+plot(household_data$DateTime1,household_data$Sub_metering_1,  type="l",ylab="Energy sub metering",xlab="")
+lines(household_data$DateTime1,household_data$Sub_metering_2,col="red")
+lines(household_data$DateTime1,household_data$Sub_metering_3,col="blue")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),cex=1,lty="solid")
 dev.off()
